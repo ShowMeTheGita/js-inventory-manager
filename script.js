0531fb53bf6cd2cce8521d3f0c2b075e8562de30
Guilherme Silva <guilherme.m.m.f.silva@gmail.com>
2020-10-01 22:35:38 +0100

    minor html changes, delete unnecessary js code for consuming data from old local server

--- script.js
+++ script.js
@@ -1,55 +1,8 @@
 $(document).ready(function () {
-    fetchProducts();
+
 })
 
-function fetchProducts() {
-    fetch("http://localhost:8080/api/products")
-        .then(response => response.json())
-        .then(function (data) {
-            data.forEach(element => {
-                $("<tr><td>" + element.name + "</td>" + "<td>" + element.quantity + "</td>" + "<td style='text-align: center'><img id=" + "'" + element.id + "'" + " onclick='deleteProduct(this.id)' src='https://upload.wikimedia.org/wikipedia/commons/4/4d/Grey_delete_icon_%28Wikiproject_icons%29.svg' alt='delete-icon' width='25px' style='cursor: pointer' /></td></tr>").appendTo('#products');
-            });
-        });
-}
 
-function submitProduct() {
-
-    var formData = $("#productform").serializeArray();
-    var toSend = {
-        name:formData[0].value,
-        quantity:formData[1].value
-    }
-
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:8080/api/addProduct",
-        data: JSON.stringify(toSend),
-        success: location.reload(),
-        dataType: "json",
-        contentType: "application/json",
-        error: (function(xhr, textStatus, errorThrown) {
-            alert('Error! Status = ' + xhr.status);
-        })
-    })
-}
-
-function deleteProduct(itemId) {
-   console.log(itemId);
-
-    $.ajax({
-        type: "DELETE",
-        url: "http://localhost:8080/api/product/delete/" + itemId,
-        data: itemId,
-        success: location.reload(),
-        dataType: "text",
-        contentType: "application/json",
-        error: (function(xhr, textStatus, errorThrown) {
-            alert('Error! Status = ' + xhr.status);
-        })
-    })
-
-
-}
 
 function filterTable() {
 
