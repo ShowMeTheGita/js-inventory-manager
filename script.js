c4cd1615b5746c9db307c107d4c40023330c88ff
Guilherme Silva <guilherme.m.m.f.silva@gmail.com>
2020-10-01 23:36:54 +0100

    add js functionality to add input data onto table dynamically

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
 
+    document.getElementById('button-add').addEventListener('click', addData)
+
 })
+
+const addData = (ev) =>{
+    
+    ev.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let quantity = document.getElementById('quantity').value;
+
+
+    let newTr = document.createElement("tr");
+    let nameTd = document.createElement("td");
+    let quantityTd = document.createElement("td");
+
+    let deleteTd = document.createElement("td");
+    deleteTd.style.textAlign = "center"
+    let imgForDeleteTd = document.createElement("img");
+    imgForDeleteTd.src = "https://upload.wikimedia.org/wikipedia/commons/4/4d/Grey_delete_icon_%28Wikiproject_icons%29.svg";
+    imgForDeleteTd.style.width = "25px";
+    imgForDeleteTd.style.height ="25px";
+    imgForDeleteTd.alt = "delete-icon";
+    imgForDeleteTd.style.cursor = "pointer";
+    deleteTd.appendChild(imgForDeleteTd);
+
+    nameTd.innerHTML = name;
+    quantityTd.innerHTML = quantity;
+
+    newTr.appendChild(nameTd);
+    newTr.appendChild(quantityTd);
+    newTr.appendChild(deleteTd);
+    document.getElementById('products').appendChild(newTr);
+
+}
 
 
 
